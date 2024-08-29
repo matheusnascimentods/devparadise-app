777386b88d66aa794ffb1269a67ba215ae48c98c
Matheuz Nascimento <matheusnascimentods@outlook.com>
2024-08-28 21:56:04 -0300

    O componente ListProjects foi descartado

--- src/components/pages/MyProjects/MyProjects.jsx
+++ src/components/pages/MyProjects/MyProjects.jsx
@@ -10,10 +10,10 @@ import styles from './MyProjects.module.css';
 //componets
 import Searchbar from '../../Searchbar/Searchbar';
 import Divider from '../../Divider/Divider';
+import Project from '../../Project/Project';
 
 //icons
 import { MdCreateNewFolder } from "react-icons/md";
-import ListProjects from '../../ListProjects/ListProjects';
 
 export default function MyProjects() {
 
@@ -32,6 +32,27 @@ export default function MyProjects() {
         });
     }, [token]);      
 
+    async function handleDelete(project) {
+        await axios.delete(`${import.meta.env.VITE_API_URL}/project/${project._id}`, {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(token)}`,
+            },
+        })
+        .then((response) => {
+            toast.success(response.data.message, {
+                position: "bottom-right",
+                theme: "dark"
+            });
+            window.location.reload();
+        })
+        .catch((error) => {
+            toast.error(error.data.message, {
+                position: "bottom-right",
+                theme: "dark"
+            });
+        });
+    }
+
     return (
         <section className={styles.my_projects_section}>
             <h2>Meus projetos</h2>
@@ -43,7 +64,9 @@ export default function MyProjects() {
                 </button>
             </div>
             <Divider/>
-            <ListProjects projects={projects} />
+            <div className={styles.list_projects}>
+                {Array.from(projects).map((project) => <Project project={project} handleDelete={handleDelete} key={project._id}/>)}
+            </div>
         </section>
     )
 }
