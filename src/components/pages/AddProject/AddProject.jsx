dd7ede3dea010313d9f8f71e47f29b49fa7a9dfa
Matheus <matheusnascimentods@outlook.com>
2024-08-29 08:54:43 -0300

    Alterações na tela addProject

--- src/components/pages/AddProject/AddProject.jsx
+++ src/components/pages/AddProject/AddProject.jsx
@@ -8,6 +8,7 @@ import axios from 'axios';
 //styles
 import styles from '../../form/Form.module.css'
 import Input from '../../Form/Input';
+import InputTags from '../../Form/TagInput';
 
 export default function AddProject() {
 
@@ -23,9 +24,16 @@ export default function AddProject() {
     project.images = e.target.files;
   }
 
+  function handleCreate(value, item) {
+    project.technologies = value;
+    console.log(project);
+    
+  }
+
   async function handleSubmit(e) {
     e.preventDefault();
     
+    console.log(project)
     let formData = new FormData();
     const projectFormData = await Object.keys(project).forEach((key) => {
       if (key === 'images') {
@@ -68,7 +76,8 @@ export default function AddProject() {
             <h1>Adicionar projeto</h1>
             <Input text='Projeto' type='text' name='title' placeholder='Informe nome do projeto' handleOnChange={handleChange}/>
             <Input text="Descrição" type='text' name='description' placeholder='Informe uma descrição sobre o seu projeto' handleOnChange={handleChange} />
-            <Input text="Imagens do projeto" type='file' name='images' handleOnChange={onFileChange} multiple={true}/>
+            <Input text="Imagens do   projeto" type='file' name='images' handleOnChange={onFileChange} multiple={true}/>
+            <InputTags text="Tecnologias" name='technologies' placeholder='Escolha as tecnologias usadas na projeto' handleOnChange={handleCreate} />
             <Input text="Repositório" type='url' name='repository' placeholder='Caso tenha um repositório desse projeto informe' handleOnChange={handleChange} />
             <input type="submit" value="Adicionar projeto" />
 
