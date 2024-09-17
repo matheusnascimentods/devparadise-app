9bfc8ab3db453c1edd83dd112d65122406d64db2
Matheuz Nascimento <matheusnascimentods@outlook.com>
2024-09-16 22:16:13 -0300

    A tela de busca está quase no final

--- src/components/pages/Searchpage/Searchpage.jsx
+++ src/components/pages/Searchpage/Searchpage.jsx
@@ -32,7 +32,7 @@ export default function Searchpage() {
             setProjects(projectsResponse.data);
             setUsers(usersResponse.data);
         }));
-    }, []);
+    });
     
     async function handleKeyDown(e) {
         if (e.key === "Enter" && e.target.value !== "") {
@@ -40,21 +40,26 @@ export default function Searchpage() {
         }
     }
 
+    console.log('projetos', projects.total);
+    console.log('usesr', users.total);
+    
+
     return (
         <section className={styles.searchpage_container}>
             <h2>Resultados encontrados para {query}</h2>
             <Searchbar placeholder='Busque por um projeto ou por outro DEV' handleKeyDown={handleKeyDown} />
             <Tabs defaultActiveKey="1" appearance="pills">
-                <Tabs.Tab eventKey="1" title="Projetos">
-                    {projects.data ? (
-                        Array.from(projects.data).map((project) => <Project project={project} key={project._id}  myProject={false}/>)
-                    ) : (<>Nada encontrado</>)}
-                </Tabs.Tab>
-                <Tabs.Tab eventKey="2" title="Desenvolvedores">
-                    {users.data ? (
-                        Array.from(users.data).map((user) => <ProfileCard user={user} />)
-                    ) : (<></>)}
-                </Tabs.Tab>
+                {projects.total > 0 && users.total === 0 ? (                    
+                    <Tabs.Tab eventKey="1" title="Projetos">
+                        {Array.from(projects.data).map((project) => <Project project={project} key={project._id}  myProject={false}/>)}
+                    </Tabs.Tab>
+                ) : (<></>)}
+                {users.total > 0 && projects.total === 0 ? (
+                    <Tabs.Tab eventKey="2" title="Desenvolvedores">
+                        {Array.from(users.data).map((user) => <ProfileCard user={user} />)}
+                    </Tabs.Tab>
+                ) : (<></>)}
+
             </Tabs>
         </section>
     )
