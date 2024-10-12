b551d3cb8fcb3944c2722b806fe0c8aaf8c8b0ff
Matheuz Nascimento <matheusnascimentods@outlook.com>
2024-10-12 18:21:15 -0300

    Refatoração no componente project

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -41,84 +41,65 @@ export default function Project({project, handleDelete, handleFavorite, myProjec
   
   return (
     <div>
-      {myProject ? (
-        <>
-          <div className={styles.project_item}>
-            <div className={styles.info}>
-              <span>
-                {project.favorite == true && (
-                  <BsPinAngleFill size={18} />
-                )}
-                <Link to={`/projects/${project._id}`}>
-                  <p>{project.title}</p>
-                </Link>
-              </span>
-              {!project.technologies || !Array.isArray(project.technologies) || project.technologies.length === 0 ? 
-              (<></>) : 
-              (
-              <div className={styles.technologies_container}>
-              <FaCircle color='ffb300' size={7} />
-              <ul>
+      <div className={styles.project_item}>
+        <div className={styles.info}>
+          <span>
+            {project.favorite == true && (
+              <BsPinAngleFill size={18} />
+            )}
+            <Link to={`/projects/${project._id}`}>
+              <p>{project.title}</p>
+            </Link>
+            </span>
+          {!project.technologies || !Array.isArray(project.technologies) || project.technologies.length === 0 ? 
+          (<></>) : 
+          (
+          <div className={styles.technologies_container}>
+            <FaCircle color='ffb300' size={7} />
+            <ul>
               {Array.from(project.technologies).map((technologie) => <li key={technologie}>{technologie}</li>)}
-              </ul>
-              </div>
-              )}
-            </div>
-            <Dropdown>
-              <Dropdown.Toggle id="dropdown-basic" variant='success'>Opções</Dropdown.Toggle>
-
-              <Dropdown.Menu>
-                <Dropdown.Item onClick={() => handleFavorite(project)}>Favoritar</Dropdown.Item>
-                <Dropdown.Item href={`/me/projects/update/${project._id}`}>Editar</Dropdown.Item>
-                <Dropdown.Item onClick={handleOpen}>Excluir</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
+            </ul>
           </div>
-          <Modal backdrop="static" role="alertdialog" open={open} onClose={handleClose} size="xs">
-            <Modal.Body>
-              <RemindIcon style={{ color: '#ffb300', fontSize: 24 }} />Realmente deseja excluir este projeto?
-            </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={handleOnClick} appearance="primary" key={project._id}>
-              Sim, excluir!
-              </Button>
-              <Button onClick={handleClose} appearance="subtle">
-              Cancelar
-              </Button>
-            </Modal.Footer>
-          </Modal>
-        </>
-      ) : (
-        <>
-          <div className={styles.project_item}>
-            <div className={styles.info}>
-              <Link to={`/projects/${project._id}`}>
-                <p>{project.title}</p>
+          )}
+        </div>
+        {!myProject ? (
+          <div className={styles.pfp}>
+            {user.image ? (
+              <Link to={`/user/${user.username}`}>
+                <RoundedImage src={`${import.meta.env.VITE_API_URL}/images/users/${user.image}`} alt="Foto de perfil" />
               </Link>
-              {!project.technologies || !Array.isArray(project.technologies) || project.technologies.length === 0 ? 
-              (<></>) : 
-              (
-              <div className={styles.technologies_container}>
-                <FaCircle color='ffb300' size={7} />
-                <ul>
-                  {Array.from(project.technologies).map((technologie) => <li key={technologie}>{technologie}</li>)}
-                </ul>
-              </div>
-              )}
-            </div>
-            <div className={styles.pfp}>
-              {user.image ? (
-                <Link to={`/user/${user.username}`}>
-                  <RoundedImage src={`${import.meta.env.VITE_API_URL}/images/users/${user.image}`} alt="Foto de perfil" />
-                </Link>
-              ) : (
-                <Link to={`/user/${user.username}`}>
-                  <RoundedImage src={defaultPfp} alt="Foto de perfil" />
-                </Link>
-              )}
-            </div>
+            ) : (
+              <Link to={`/user/${user.username}`}>
+                <RoundedImage src={defaultPfp} alt="Foto de perfil" />
+              </Link>
+            )}
           </div>
-        </>
+        ) : (
+          <Dropdown>
+            <Dropdown.Toggle id="dropdown-basic" variant='success'>Opções</Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={() => handleFavorite(project)}>Favoritar</Dropdown.Item>
+              <Dropdown.Item href={`/me/projects/update/${project._id}`}>Editar</Dropdown.Item>
+              <Dropdown.Item onClick={handleOpen}>Excluir</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        )}
+      </div>
+      {myProject && (
+        <Modal backdrop="static" role="alertdialog" open={open} onClose={handleClose} size="xs">
+          <Modal.Body>
+            <RemindIcon style={{ color: '#ffb300', fontSize: 24 }} />Realmente deseja excluir este projeto?
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={handleOnClick} appearance="primary" key={project._id}>
+            Sim, excluir!
+            </Button>
+            <Button onClick={handleClose} appearance="subtle">
+            Cancelar
+            </Button>
+          </Modal.Footer>
+        </Modal>
       )}
     </div>
   )
